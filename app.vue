f9f7fa6b6ef54ec0b3939a461a9fe2f6a01af480
Safiull Alam <safiullalam9931@gmail.com>
2023-07-25 11:19:34 +0600

    component loading.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <HelloParagraph/>
   </div>
 </template>
---
+++ components/Hello/paragraph.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima pariatur, reprehenderit molestias hic vitae ratione repellendus debitis beatae veritatis ad voluptatibus similique eveniet delectus odit et magni nostrum quos veniam!</p>
+</template>
